c36064c6c2e8b2a87c525bf0ae5326068130a283
Seongbin Kim <seongbin9786@gmail.com>
2023-12-17 22:25:43 +0900

    chore: demo package 생성 (Vite)

---
+++ packages/demo/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    env: { browser: true, es2020: true },
+    extends: ["plugin:react-hooks/recommended"],
+    plugins: ["react-refresh"],
+    rules: {
+        "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+};
